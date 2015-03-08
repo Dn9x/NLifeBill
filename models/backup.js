3946123b40e0ec6dae799ff827de48429025738c
Dn9x <xiuxu123@live.cn>
2015-03-08 15:09:34 +0800

    给权限

--- models/backup.js
+++ models/backup.js
@@ -7,24 +7,32 @@ var Backup = {};
 
 module.exports = Backup;
 
-Backup.send = function(){
+Backup.send = function() {
 	var date = new Date();
 
-	var time = date.getFullYear() + "" + (date.getMonth()+1) + ""  + date.getDate() 
-		+ date.getHours() + ""  + date.getMinutes() + ""  + date.getSeconds(); 
+	var time = date.getFullYear() + "" + (date.getMonth() + 1) + "" + date.getDate() + date.getHours() + "" + date.getMinutes() + "" + date.getSeconds();
 
-	var shell = "mysqldump -u root -p1234 lifebill > bak/lb"+time+".sql";
+	var shell = "mysqldump -u root -p1234 lifebill > bak/lb" + time + ".sql";
 
-	process.exec(shell, function (error, stdout, stderr) {
-	    //console.log('stdout: ' + stdout);
-	    //console.log('stderr: ' + stderr);
-	    if (error !== null) {
-	      console.log('exec error: ' + error);
-	    }
+	process.exec(shell, function(error, stdout, stderr) {
+		// console.log("error: " + error);
+		//    console.log('stdout: ' + stdout);
+		//    console.log('stderr: ' + stderr);
 
-	    console.log(time + "：已经备份成功");
+		var shell = "chmod 777 bak/lb" + time + ".sql";
+		console.log("shell: " + shell);
 
-	    upload("lb"+time+".sql");
+		process.exec(shell, function(error, stdout, stderr) {
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+			if (error !== null) {
+				console.log('exec error: ' + error);
+			}
+
+			console.log(time + "：已经备份成功");
+
+			upload("lb" + time + ".sql");
+		});
 	});
 }
 
@@ -42,52 +50,49 @@ Backup.send = function(){
 
 // });
 
-function upload(filename){
+function upload(filename) {
 
 	var date = new Date();
-	var time = date.getFullYear() + "-" + (date.getMonth()+1) + "-"  + date.getDate() + " "
-		+ date.getHours() + ":"  + date.getMinutes(); 
+	var time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes();
 
 	//获取地址信息
 	var address = os.networkInterfaces();
 	var totalmem = os.totalmem();
 	var freemem = os.freemem();
 
-	var msg = "<p>总内存量：" + totalmem + "</p><p>空闲内存量：" + freemem + "</p><p>地址信息：<pre>" + 
+	var msg = "<p>总内存量：" + totalmem + "</p><p>空闲内存量：" + freemem + "</p><p>地址信息：<pre>" +
 		JSON.stringify(address) + "</pre></p>";
 
 	// create reusable transport method (opens pool of SMTP connections)
-	var smtpTransport = nodemailer.createTransport("SMTP",{
-	    service: "QQ",
-	    auth: {
-	        user: "171373243@qq.com",
-	        pass: "307zhanghanyun"
-	    }
+	var smtpTransport = nodemailer.createTransport("SMTP", {
+		service: "QQ",
+		auth: {
+			user: "171373243@qq.com",
+			pass: "307zhanghanyun"
+		}
 	});
 
 
 	// setup e-mail data with unicode symbols
 	var mailOptions = {
-	    from: "171373243@qq.com", // sender address
-	    to: "xiuxu123@live.cn, 932578775@qq.com", // list of receivers
-	    subject: "LifeBill备份：" + time, // Subject line
-	    text: "LifeBill备份" + time, // plaintext body
-	    html: msg, // html body
-	    attachments: [
-	        {   // stream as an attachment
-	            fileName: filename,
-	            streamSource: fs.createReadStream("bak/" + filename)
-	        }
-	    ]
+		from: "171373243@qq.com", // sender address
+		to: "xiuxu123@live.cn, 932578775@qq.com", // list of receivers
+		subject: "LifeBill备份：" + time, // Subject line
+		text: "LifeBill备份" + time, // plaintext body
+		html: msg, // html body
+		attachments: [{ // stream as an attachment
+			fileName: filename,
+			streamSource: fs.createReadStream("bak/" + filename)
+		}]
 	}
 
 	// send mail with defined transport object
-	smtpTransport.sendMail(mailOptions, function(error, response){
-	    if(error){
-	        console.log(error);
-	    }else{
-	        console.log("Message sent: " + response.message);
-	    }
+	smtpTransport.sendMail(mailOptions, function(error, response) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log("Message sent: " + response.message);
+		}
 	});
 
 }
